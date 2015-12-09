6953f37ff9f4975f5fc920813ff7cc61440d3884
Excrulon <excrulon@gmail.com>
2015-12-08 23:25:27 -0500

    set title bar

--- main.js
+++ main.js
@@ -25,6 +25,8 @@ app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
+  mainWindow.setTitle("Tree of Savior Mod Manager");
+
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
