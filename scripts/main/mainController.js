c00bd11dfebe9e0294ecff36ac4963107ea4b994
Excrulon <excrulon@gmail.com>
2015-12-08 22:09:54 -0500

    added angular material

---
+++ scripts/main/mainController.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    angular.module('app')
+        .controller('mainController', [MainController]);
+
+    function CustomerController(customerService, $q, $mdDialog) {
+    }
+
+})();
