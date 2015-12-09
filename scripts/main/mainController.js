7236fbb0ee75e99584dca1f78d74de8d46317fb8
Excrulon <excrulon@gmail.com>
2015-12-08 23:18:51 -0500

    added a select list for mods

--- scripts/main/mainController.js
+++ scripts/main/mainController.js
@@ -1,7 +1,11 @@
 (function () {
-    'use strict';
-    angular.module('app')
-        .controller('mainController', function($scope) {
+	'use strict';
+	angular.module('app')
+	.controller('mainController', function($scope) {
 
-		});
+		$scope.mods = [
+			{ name: 'expviewer', author: 'Excrulon', isInstalled: false },
+			{ name: 'mobframes', author: 'SecurityRisk', isInstalled: false }
+		];
+	});
 })();
