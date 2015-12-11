b0e2fe626a96b8cb840a38ab35621020497a17ea
Excrulon <excrulon@gmail.com>
2015-12-11 16:46:54 -0500

    only show progress bar if downloading

--- scripts/install/install.controller.js
+++ scripts/install/install.controller.js
@@ -1,26 +1,26 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('app')
-        .controller('InstallController', InstallController);
+	angular
+	.module('app')
+	.controller('InstallController', InstallController);
 
-    InstallController.$inject = ['$scope', 'AddonFactory'];
+	InstallController.$inject = ['$scope', 'AddonFactory'];
 
-    /* @ngInject */
-    function InstallController($scope, AddonFactory) {
-        var viewModel = this;
+	/* @ngInject */
+	function InstallController($scope, AddonFactory) {
+		var viewModel = this;
 		viewModel.addons = [];
 		viewModel.getAddons = getAddons;
 		viewModel.installMod = installMod;
 		viewModel.treeOfSaviorDirectory = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\TreeOfSavior\\";
 
-        activate();
+		activate();
 
-        function activate() {
+		function activate() {
 			return getAddons().then(function() {
 			});
-        }
+		}
 
 		function getAddons() {
 			return AddonFactory.getAddons().then(function(data) {
@@ -31,6 +31,7 @@
 
 		function installMod(mod) {
 			mod.url = "http://speedtest.atlanta.linode.com/100MB-atlanta.bin";
+			mod.isDownloading = true;
 
 			var modDirectory = viewModel.treeOfSaviorDirectory + "mods\\";
 			var filename = modDirectory + mod.name + ".zip";
@@ -42,21 +43,30 @@
 			var request = http.get(mod.url, function(response) {
 				var len = parseInt(response.headers['content-length'], 10);
 				var body = "";
-            	var cur = 0;
+				var cur = 0;
 				var BYTES_IN_MEGABYTE = 1048576;
 				var total = len / BYTES_IN_MEGABYTE;
 
 				response.on("data", function(chunk) {
-	                body += chunk;
-	                cur += chunk.length;
+					body += chunk;
+					cur += chunk.length;
 					var percent = (100.0 * cur / len).toFixed(2);
-					console.log("Downloading " + percent + "% " + (cur / 1048576).toFixed(2) + " mb\r" + ". Total size: " + total.toFixed(2) + " mb");
+					//console.log("Downloading " + percent + "% " + (cur / 1048576).toFixed(2) + " mb\r" + ". Total size: " + total.toFixed(2) + " mb");
 
 					mod.installProgress = percent;
-	            });
-				console.log(response);
+					console.log("prog: " + mod.installProgress);
+				});
+
+				response.on("end", function() {
+					mod.isDownloading = false;
+				});
+
+				request.on("error", function(error) {
+					mod.isDownloading = false;
+				});
+
 				response.pipe(file);
 			});
 		}
-    }
+	}
 })();
