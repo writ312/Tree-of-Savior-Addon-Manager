1ed87da64bf502ed64b8827eada060157f978aa2
Excrulon <excrulon@gmail.com>
2015-12-11 01:20:03 -0500

    removed dead code.

--- scripts/app.js
+++ scripts/app.js
@@ -10,10 +10,6 @@
         'ngAnimate'
     ])
     .config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
-			// $mdThemingProvider.theme('default')
-			//     .primaryPalette('pink')
-			//     .accentPalette('orange');
-
 			$mdThemingProvider.theme('default')
 				.dark();
 
@@ -30,56 +26,6 @@
 					controller: 'InstallController as installController',
 					templateUrl: _templateBase + '/install/install.html'
 				});
-
-			/*
-			.state('tabs', {
-				abstract: true,
-				url: '/',
-				templateUrl: 'template.html',
-				controller: function($scope) {
-					$scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-						$scope.currentTab = toState.data.selectedTab;
-					});
-				}
-			})
-			.state('tabs.settings', {
-				url: '/settings',
-				templateUrl: _templateBase + '/settings/settings.html',
-				data: {
-					'selectedTab': 0
-				},
-				views: {
-					'settings': {
-						controller: 'SettingsController'
-					}
-				}
-			})
-			.state('tabs.install', {
-				url: '/install',
-				data: {
-					'selectedTab': 1
-				},
-				views: {
-					'install': {
-						controller: 'InstallController'
-					}
-				}
-			});
-			*/
-
-			/*
-            $routeProvider.when('/', {
-                templateUrl: _templateBase + '/main/main.html' ,
-                controller: 'MainController',
-                controllerAs: 'mainController'
-            }).when('/settings', {
-				templateUrl: _templateBase + '/settings/settings.html' ,
-                controller: 'SettingsController',
-                controllerAs: 'settingsController'
-			}).otherwise({
-				redirectTo: '/'
-			});
-			*/
         }
     ]);
 })();
