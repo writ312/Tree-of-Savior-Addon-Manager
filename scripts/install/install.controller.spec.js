4fd879ee3188b3c6f2e49dcd43cc65853faf3945
Excrulon <excrulon@gmail.com>
2015-12-11 22:34:51 -0500

    refactored test to clean up controller instantiation

--- scripts/install/install.controller.spec.js
+++ scripts/install/install.controller.spec.js
@@ -1,12 +1,15 @@
 describe("Install Controller", function() {
 
+	var installController;
+
 	beforeEach(module('app'));
+	beforeEach(inject(function($controller) {
+		installController = $controller('InstallController', {
+			$scope: {}
+		});
+	}));
 
 	it('getAddons method should be defined', inject(function($controller) {
-        var ctrl = $controller('InstallController', {
-			$scope : {}
-		});
-
-		expect(ctrl.getAddons).toBeDefined();
-    }));
+		expect(installController.getAddons).toBeDefined();
+	}));
 });
