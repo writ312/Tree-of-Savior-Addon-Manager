99da5a0472d23ae4f33a9c41574ce2f4032b6c4b
Excrulon <excrulon@gmail.com>
2016-05-29 02:24:37 -0400

    removed unused code

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,13 @@
 (function () {
     'use strict';
 
-    var _templateBase = './scripts';
-
     angular.module('app', [
         'ui.router',
 		'ngResource',
         'ngMaterial',
         'ngAnimate'
     ])
-    .config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
-			//$mdThemingProvider.theme('default')
-				//.dark();
-
+    .config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider) {
 			$urlRouterProvider.otherwise("/tab");
 
 			$stateProvider
@@ -36,25 +31,6 @@
 					templateUrl: 'views/installed.html',
 					controller: 'InstalledController as installedController'
 				});
-
-				/*
-			$stateProvider
-				.state('main', {
-					url: '/',
-					controller: 'MainController as mainController',
-					templateUrl: _templateBase + '/main/main.html'
-				});
-				.state('settings', {
-					url: '/settings',
-					controller: 'SettingsController as settingsController',
-					templateUrl: _templateBase + '/settings/settings.html'
-				})
-				.state('install', {
-					url: '/install',
-					controller: 'InstallController as installController',
-					templateUrl: _templateBase + '/install/install.html'
-				});
-				*/
         }
     ]);
 })();
