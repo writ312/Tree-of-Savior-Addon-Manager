da22a62e3f0dfd9ab5bedc76816c6ac63543dafb
Excrulon <excrulon@gmail.com>
2016-05-30 04:38:14 -0400

    setting flag for if an update is available based on the semver and added cache busting.

--- scripts/browse/browse.controller.js
+++ scripts/browse/browse.controller.js
@@ -13,14 +13,14 @@
 		var masterSources = "https://raw.githubusercontent.com/Tree-of-Savior-Addon-Community/Addons/master/addons.json";
 
 		settings.getInstalledAddons(function(installedAddons) {
-			$http.get(masterSources).success(function(data) {
+			$http.get(masterSources + "?" + new Date().toString(), {cache: false}).success(function(data) {
+				console.log(JSON.stringify(data));
 				$log.info("Loading master sources from " + masterSources);
 				var addons = [];
 				angular.forEach(data.sources, function(source) {
-
 					var repo = `https://raw.githubusercontent.com/${source.repo}/master/addons.json`;
-
-					$http.get(repo).success(function(sourceData) {
+					
+					$http.get(repo + "?" + new Date().toString()).success(function(sourceData) {
 						angular.forEach(sourceData, function(addon) {
 
 							var repoValues = source.repo.split('/');
@@ -38,9 +38,16 @@
 							addon.isDownloading = false;
 
 							var installedAddon = installedAddons[addon.file];
-							
+
 							if(installedAddon) {
 								addon.isInstalled = true;
+								var semver = require('semver');
+
+								$log.info("Update available for " + addon.name + "? " + semver.gt(addon.fileVersion, installedAddon.fileVersion));
+
+								if(semver.gt(addon.fileVersion, installedAddon.fileVersion)) {
+									addon.isUpdateAvailable = true;
+								}
 							}
 
 							addons.push(addon);
